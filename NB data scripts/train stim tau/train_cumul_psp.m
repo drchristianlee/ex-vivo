1c7e721c4495ea3e27096eea7ec3c982bd4b1677
Christian Lee <christian.r.lee@gmail.com>
2019-03-05 14:29:54 -0500

    Update train_cumul_psp.m

--- NB data scripts/train stim tau/train_cumul_psp.m
+++ NB data scripts/train stim tau/train_cumul_psp.m
@@ -1,2 +1,21 @@
 % use this script to analyze cumulative normalized psp data
 
+clear;
+folder = uigetdir;
+cd(folder);
+filePattern = fullfile(folder, '*.mat');
+matfiles = dir(filePattern);
+count = length(matfiles);
+
+for f = 1:count;
+    B = matfiles(f, 1).name;
+    currkeeper = load(B);
+    result_holder(f, :) = currkeeper.cumul;
+end
+
+
+for plot_step = 1:size(result_holder, 1);
+    plot(result_holder(plot_step, :));
+    hold on;
+end
+
