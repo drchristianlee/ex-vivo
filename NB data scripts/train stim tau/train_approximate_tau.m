dbdbd09045af25b99f41279f4bcd0a01ace1b476
Christian Lee <christian.r.lee@gmail.com>
2019-03-05 14:29:52 -0500

    Update train_approximate_tau.m

--- NB data scripts/train stim tau/train_approximate_tau.m
+++ NB data scripts/train stim tau/train_approximate_tau.m
@@ -14,7 +14,7 @@ res(1:size(holder, 1), 1) = zeros;
 for sample = 1:size(holder, 1);
     stepper = 1;
     for pulse = 1:size(holder, 2);
-        if holder(sample, pulse) < 50;
+        if holder(sample, pulse) < 36.8;
             res(sample, stepper) = pulse;
             stepper = stepper + 1;
         else
@@ -27,13 +27,19 @@ res(res == 0)= 50;
 file_name = path(end-8 : end-1);
 save([file_name '_tau'] , 'res');
 
-avg = nanmean(holder, 1);
-for pulse = 1:50;
-    if pulse == 1;
-        cumul = avg(1, 1);
+for cell = 1:size(holder, 1);
+for stim = 1:50;
+    if stim == 1;
+        cumul(cell, 1) = holder(cell, 1);
     else
-        cumul(1, pulse) = avg(1, pulse) + cumul(1, pulse -1);
+        cumul(cell, stim) = holder(cell, stim) + cumul(cell, stim -1);
     end
 end
+end
+
+cumul_plot(1, :) = colon(1, 50);
+cumul_plot(2, :) = nanmean(cumul, 1);
+cumul_plot(3, :) = nanstd(cumul, 1) / (sqrt(size(cumul, 1)));
 
 save([file_name 'cumul'] , 'cumul');
+save([file_name 'cumul_plot'] , 'cumul_plot');
