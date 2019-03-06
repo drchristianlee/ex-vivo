4eac6bc7858de2f54b7b6bc0b4712b7eaa2ebdbd
Christian Lee <christian.r.lee@gmail.com>
2019-03-06 09:34:49 -0500

    Update train_cumul_compare.m

--- NB data scripts/train stim tau/train_cumul_compare.m
+++ NB data scripts/train stim tau/train_cumul_compare.m
@@ -4,14 +4,27 @@ clear;
 close all
 clc
 
-[path] = uigetdir;
-cd(path);
+folder = uigetdir;
+cd(folder);
+filePattern = fullfile(folder, '*.mat');
+matfiles = dir(filePattern);
+count = length(matfiles);
 
-load('S1.mat') %choose which file to load
+for f = 1:count;
+    B = matfiles(f, 1).name;
+    currkeeper = load(B);
+    result_holder(1, f) = {currkeeper.cumul};
+end
+
+for grouper = 1:size(result_holder, 2);
+    extractor = result_holder{1, grouper}(:, end);
+    for stepper = 1:size(extractor, 1);
+    test_keeper(stepper, grouper) = extractor(stepper, 1);
+    end
+end
 
 test_keeper(test_keeper == 0) = NaN;
 
-test_keeper = test_keeper * 1000;
 
 barkeeper(1,1) = nanmean(test_keeper(:,1));
 barkeeper(1,2) = nanmean(test_keeper(:, 2));
@@ -38,7 +51,7 @@ end
 errorbar(barkeeper(1,:), barkeeper(2,:), '.', 'color', 'k', 'marker', 'none');
 
 
- axis([0 4 0 40])
+ axis([0 4 0 10000])
  set(gca,'TickDir','out')
  set(gca, 'box', 'off')
  set(gcf,'position',[680 558 200 410])
