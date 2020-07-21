1852366c3e7b24d46d8450e4d2da7383e896bc40
Christian Lee <christian.r.lee@gmail.com>
2020-07-21 19:18:08 +0000

    Update import_pm_next_native.m

--- Patchmaster Next/import_pm_next_native.m
+++ Patchmaster Next/import_pm_next_native.m
@@ -132,5 +132,5 @@ set(ax2,'FontSize',9);
 %to save figure in high resolution format
 % print -painters -depsc output.eps 
 
-file_nm = [ans.opt.filepath(end-17 : end-8) , '_results.mat']
-save(file_nm , result)
+file_nm = [ans.opt.filepath(end-17 : end-8) , '_cell_' , num2str(cell(1,1)) , '_results.mat']
+save(file_nm , 'result')
