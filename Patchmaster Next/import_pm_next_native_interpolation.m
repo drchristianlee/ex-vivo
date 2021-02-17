9521cc66a7983d1ffd2cbb6572f10e0036fd21c7
Christian Lee <christian.r.lee@gmail.com>
2021-02-17 17:34:47 +0000

    Update import_pm_next_native_interpolation.m

--- Patchmaster Next/import_pm_next_native_interpolation.m
+++ Patchmaster Next/import_pm_next_native_interpolation.m
@@ -133,7 +133,7 @@ if analyze == 1;
         idx_1 = find(trace_interp(thresh_idx:thresh_idx + 250, 1) > half_amp);
         half_width_interp(1, spike) = (1/(data.RecTable.SR(manipulation)*5)) * size(idx_1, 1);
         ahp_interp(1, spike) = threshold_interp(1, spike) - (min(trace_interp(thresh_idx:thresh_idx + 500, 1))); %might need to modify for spikes near end of pulse
-        [min, I] = min(trace_interp(thresh_idx:thresh_idx + 50, 1));
+        [ahp_hold, I] = min(trace_interp(thresh_idx:thresh_idx + 500, 1));
         ahp_ind = thresh_idx + I;
         dV{1, spike} = slope(thresh_idx:ahp_ind, 1);
         Vm{1, spike} = trace_interp(thresh_idx:ahp_ind, 1);
